3ffda370c212be52575b0e81cf96a7c2556ed7ee
Markus <loew@mytum.de>
2017-08-25 12:03:46 +1000

    Initial version

---
+++ R/MyDistOrd.R
@@ -0,0 +1,45 @@
+#' Function to re-order a symmetric additive relationship matrix created by \code{sommer::A.mat} following the algorithm in heatmap().
+#'
+#' @param x Symmetric additive relationship matrix (as created by \code{sommer::A.mat})
+#' @return Provides the sorted matrix.
+#' @export
+#' @examples
+#' require(sommer)
+#' x <- matrix(rnorm(50), 10, 10, 
+#'            dimnames = list(paste("g", 1:10, sep=""), 
+#'            paste("t", 1:10, sep=""))) 
+#' ## convert to additive relationship matrix
+#' x <- sommer::A.mat(x)
+#' hv <- heatmap(x)
+#' heatmap.sorted <- x[rev(hv$rowInd), rev(hv$colInd)]
+#' sorted <- MyDistOrd(x)
+#' ## to show that the result of heatmap() and this function are the same
+#' heatmap.sorted == sorted
+
+MyDistOrd <- function(x) {
+  # Markus Loew, Aug 25, 2017
+  # only works for a symmetric matrix - see comment below
+    
+  stopifnot(class(x) == "matrix")
+  stopifnot(nrow(x) == ncol(x))
+
+  Rowv   <- rowMeans(x, na.rm = TRUE)
+  hcr    <- stats::hclust(stats::dist(x))
+  ddr    <- stats::as.dendrogram(hcr)
+  ddr    <- stats::reorder(ddr, Rowv)
+
+  rowInd <- stats::order.dendrogram(ddr)
+
+  # columns (A needs to be symmetric!)
+  # this whole bit is pretty much redundant, as the sorting only works
+  # when x is symmetric - still here for comparison with heatmap()
+  Colv   <- Rowv  # see Colv = if (symm) "Rowv" else NULL in heatmap
+  hcc    <- stats::hclust(stats::dist(x))
+  ddc    <- stats::as.dendrogram(hcc)
+  ddc    <- stats::reorder(ddc, Colv)
+  colInd <- stats::order.dendrogram(ddc)
+
+  # sort
+  my.sorted <- x[rev(rowInd), rev(colInd)]
+  return(my.sorted)
+}
---
+++ Readme.md
@@ -0,0 +1,51 @@
+[![Build Status](https://travis-ci.org/MarkusLoew/MyDistOrd.svg?branch=master)](https://travis-ci.org/MarkusLoew/CampbellLogger)
+
+
+
+MyDistOrd
+==============
+
+R-package to re-order an additive relationship matrix (created by *sommer::A.mat()*) following the algorithm used in *stats::heatmap()* but without the overhead of creating a figure. Benefiit over *heatmap()* is that it will handle large matrices.
+
+See 
+
+	help(package = "MyDistOrd") 
+
+for details on the function provided by this package.
+
+### Installation
+
+To install this package straight from github, the "devtools" package is needed:
+
+```r
+install.packages("devtools")
+```
+
+Then, package installation via
+
+```{r}
+devtools::install_github("MarkusLoew/MyDistOrd")
+```
+
+Installation under Windows might require the installation of Rtools. There will be a prompt for it if needed.
+
+### Example usage  
+# shows that this function results in the same ordering as heatmap()  
+
+```{r}
+require(sommer) # to create a additive relationship matrix
+
+## create an example matrix
+x <- matrix(rnorm(50), 10, 10, 
+           dimnames = list(paste("a", 1:10, sep=""), 
+           paste("b", 1:10, sep=""))) 
+
+## convert to additive relationship matrix
+x <- sommer::A.mat(x)
+hv <- heatmap(x)
+heatmap.sorted <- x[rev(hv$rowInd), rev(hv$colInd)]
+sorted <- MyDistOrd(x)
+
+## to show that the result of heatmap() and this function are the same
+heatmap.sorted == sorted
+```
